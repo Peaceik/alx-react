991c8c13992bd651ee9601a1a1d5f624c6c91e28
peaceik <igwudup@gmail.com>
2023-06-08 12:45:29 -0700

    immutable

---
+++ 0x06-React_Immutable/5-main.js
@@ -0,0 +1,8 @@
+import { concatElements, mergeElements } from './5-merge';
+
+const arr1 = ['one', 'two'];
+const arr2 = ['three', 'four'];
+
+const obj1 = {
+  fear: true,
+  smell: -1033575916.9145899,
